ad94721cdd1b4d132919967d1e1599849e813c06
Arifuljava <arifulislam.java.c@gmail.com>
2023-05-29 14:42:33 +0600

    ass

---
+++ app/src/main/java/com/grozziie/atcproject/MainCoin.java
@@ -0,0 +1,19 @@
+package com.grozziie.atcproject;
+public class MainCoin {
+    String coin;
+
+    public MainCoin() {
+    }
+
+    public MainCoin(String coin) {
+        this.coin = coin;
+    }
+
+    public String getCoin() {
+        return coin;
+    }
+
+    public void setCoin(String coin) {
+        this.coin = coin;
+    }
+}
---
+++ app/src/main/java/com/grozziie/atcproject/Video1Activity.java
@@ -0,0 +1,14 @@
+package com.grozziie.atcproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Video1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video1);
+    }
+}
